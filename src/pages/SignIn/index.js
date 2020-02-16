bfd361b9f1c413057a2fb1c5ef8096eac40f4d0f
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2020-02-16 19:31:20 -0300

    Creates sign in layout

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,63 @@
-import React from 'react';
-
-import { View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import {
+  View,
+  Platform,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 import styles from './styles';
 
-const SignIn = () => <View style={styles.container} />;
+export default function SignIn() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-export default SignIn;
+  const refPassword = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform === 'ios' ? 'padding' : null}
+      style={styles.container}
+    >
+      <View>
+        <Text style={styles.title}>Entrar</Text>
+
+        <Text style={styles.label}>E-MAIL</Text>
+        <TextInput
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          autoFocus
+          returnKeyType="next"
+          onSubmitEditing={() => refPassword.current.focus()}
+        />
+
+        <Text style={styles.label}>SENHA</Text>
+        <TextInput
+          ref={refPassword}
+          value={password}
+          onChangeText={setPassword}
+          style={styles.input}
+          secureTextEntry
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
+        />
+
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
