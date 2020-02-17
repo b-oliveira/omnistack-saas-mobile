b02b3399ffebf3d57e2c9b4fb0dfb8de7aae3d2c
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2020-02-16 22:32:47 -0300

    Creates the main page layout

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { View } from 'react-native';
+import styles from './styles';
 
-// import { Container } from './styles';
-
-const Main = () => <View />;
-
-export default Main;
+export default function Main() {
+  return (
+    <View style={styles.backgroundWrapper}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity
+            hitSlop={{ top: 5, bottom: 5, left: 10, right: 10 }}
+            onPress={() => {}}
+          >
+            <Icon name="menu" size={24} color="#fff"></Icon>
+          </TouchableOpacity>
+          <Text style={styles.teamTitle}>Rocketseat</Text>
+          <TouchableOpacity
+            hitSlop={{ top: 5, bottom: 5, left: 10, right: 10 }}
+            onPress={() => {}}
+          >
+            <Icon name="group" size={24} color="#fff"></Icon>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
