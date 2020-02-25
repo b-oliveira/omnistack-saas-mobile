0df9e1897da36f619ec550a4dc19942b26da82ee
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2020-02-25 20:11:47 -0300

    Fix check access rules

--- src/components/Can/index.js
+++ src/components/Can/index.js
@@ -4,7 +4,7 @@ export default function Can({ children, role, permission }) {
   const { roles, permissions } = useSelector(state => state.team);
 
   function checkAccessRules() {
-    if (!role && !role) return false;
+    if (!role && !permission) return false;
 
     if (role && !roles.includes(role)) return false;
 
